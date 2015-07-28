f4269280ecee9d3b58b7a94bcd521f7c437873f6
Hephaestos <dannyboyinpy@gmail.com>
2015-07-28 15:20:19 -0400

    added icons for monsters

--- monsters.py
+++ monsters.py
@@ -1,5 +1,5 @@
 class Monster(object):
-	def __init__(self, speed, armor, health, attack):
+	def __init__(self, speed, armor, health, attack, icon):
 		self.speed = speed
 		self.armor = armor
 		self.health = health
@@ -12,16 +12,16 @@ class Orc(Monster):
 	"""docstring for Orc"""
 	def __init__(self):
 		print("created an Orc")
-		super(Orc, self).__init__(7, 3, 2, 4)
+		super(Orc, self).__init__(7, 3, 2, 4, 'o')
 
 class Monkey(Monster):
 	"""docstring for Monkey"""
 	def __init__(self):
 		print("created an Monkey")
-		super(Monkey, self).__init__(5, 3, 4, 5)
+		super(Monkey, self).__init__(5, 3, 4, 5, 'm')
 
 class Dragon(Monster):
 	"""docstring for Dragon"""
 	def __init__(self):
 		print("created an Dragon")
-		super(Dragon, self).__init__(3, 7, 10, 7)
+		super(Dragon, self).__init__(3, 7, 10, 7, 'D')
