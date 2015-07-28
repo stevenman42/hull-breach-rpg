e396031cc8f546dbcc0d011e37c38532c04cf9ba
Hephaestos <dannyboyinpy@gmail.com>
2015-07-28 13:57:59 -0400

    added monster class and orc class

--- characters.py
+++ characters.py
@@ -1,6 +1,6 @@
 
 class Character(object):
-	def __init__(self, speed, armor, health, attack):
+	def __init__(self, speed, armor, health, attack,hunger):
 		self.speed = speed
 		self.armor = armor
 		self.health = health
@@ -9,9 +9,17 @@ class Character(object):
 	def attack(self,object,damage):
 		object.health -= damage
 
+class Monster(object):
+	def __init__(self, speed, armor, health, attack):
+		self.speed = speed
+		self.armor = armor
+		self.health = health
+		self.armor = attack
+
 class Knight(Character):
 	"""docstring for Knight"""
 	def __init__(self):
+		print("created a Knight")
 		super(Knight, self).__init__(3, 8, 6, 5)
 
 class Wizard(Character):
@@ -30,4 +38,10 @@ class Roag(Character):
 	"""docstring for Wizard"""
 	def __init__(self):
 		print("created a Roag")
-		super(Wizard, self).__init__(10, 5, 8, 2)
+		super(Roag, self).__init__(10, 5, 8, 2)
+
+class Orc(Monster):
+	"""docstring for Orc"""
+	def __init__(self):
+		print("created an Orc")
+		super(Orc, self).__init__(7, 3, 2, 4)
