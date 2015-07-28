f937fac614d686c166d9220c5a08a8901e393c2d
johnyburd <johnyburd8@gmail.com>
2015-07-27 23:30:23 -0400

    menu

--- title.py
+++ title.py
@@ -1,7 +1,22 @@
-menu = ['create new game', '1', 'continue game', '2', 'settings', '3', 'exit', '4']
+menu = ['create new game (1)', 'continue game (2)', 'settings (3)', 'exit (4)']
 
 def title():
     for item in menu:
-        print(item)
+        print item
+    selection = str(input())
+
+
+    if selection == '1':
+        print("start new game")
+    elif selection == '2':
+        print("continue")
+    elif selection == '3':
+        print("settings")
+    elif selection == '4':
+        print("bye!")
+        quit()
+
+    else:
+        title()
 
 title()
