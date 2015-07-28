19f215807e6ba5d30f87ea160099c6d52c08aa60
Hephaestos <dannyboyinpy@gmail.com>
2015-07-27 23:20:10 -0400

    Character Classes

---
+++ characters.py
@@ -0,0 +1,15 @@
+class Knight(object):
+	"""docstring for Knight"""
+	def __init__(self, arg):
+		super(Knight, self).__init__()
+		self.speed = 3
+		self.armor = 8
+		self.health = 6
+
+class Wizard(object):
+	"""docstring for Wizard"""
+	def __init__(self, arg):
+		super(Wizard, self).__init__()
+		self.speed = 5
+		self.armor = 3
+		self.health = 8
